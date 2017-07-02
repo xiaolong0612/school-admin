82b85712c582adbb68bedd0301dc91f3091c1334
LR <1057520381@qq.com>
2017-07-02 17:05:24 +0800

    api文件

---
+++ src/api/index.js
@@ -0,0 +1,18 @@
+import fetch from 'utils/fetch';
+/**
+ * @author xiaolong
+ * @default 首页
+ */
+export function student() {
+  return fetch({
+    url: '',
+    method: 'post',
+  });
+}
+
+export function teacher() {
+  return fetch({
+    url: '',
+    method: 'post',
+  });
+}
---
+++ src/api/special.js
@@ -0,0 +1,6 @@
+import fetch from 'utils/fetch';
+/**
+ * @Author    xiaolongjun
+ * @DateTime  2017-07-01
+ * @namespace 专题
+ *／
